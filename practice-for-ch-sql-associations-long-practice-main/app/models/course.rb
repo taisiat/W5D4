5c2e248e546e7139c525576924d3459a6d6b884d
Taisia Karaseva <taisiat@gmail.com>
2023-03-02 15:33:53 -0800

    Add initial associations for enrollment and user

---
+++ practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint           not null, primary key
+#  name          :string           not null
+#  prereq_id     :bigint
+#  instructor_id :bigint           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+class Course < ApplicationRecord
+
+    has_many :enrollments,
+        primary_key: :id,
+        foreign_key: :course_id,
+        class_name: :Enrollment
+end
