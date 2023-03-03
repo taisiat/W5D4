d9ee1b0781d823fb858fd30044d0c87696400ce7
Kathy Vu <vu.kathy98@gmail.com>
2023-03-02 18:03:17 -0800

    add shortened_url

--- practice-for-ch-sql-url-shortener-long-practice-main/URLShortener/app/models/shortened_url.rb
+++ practice-for-ch-sql-url-shortener-long-practice-main/URLShortener/app/models/shortened_url.rb
@@ -1,2 +1,40 @@
+# == Schema Information
+#
+# Table name: shortened_urls
+#
+#  id         :bigint           not null, primary key
+#  long_url   :string           not null
+#  short_url  :string
+#  user_id    :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class ShortenedUrl < ApplicationRecord
+    validates :short_url, uniqueness: true
+    validates :long_url, presence: true
+
+    after_initialize do |shortened_url|
+        if self.new_record?
+            self.generate_short_url
+        end
+    end
+
+    belongs_to :submitter,
+        primary_key: :id,
+        foreign_key: :user_id,
+        class_name: :User
+
+    def random_code
+        encoding = SecureRandom.urlsafe_base64
+        while ShortenedUrl.exists?(:short_url => encoding)
+            encoding = SecureRandom.urlsafe_base64
+        end
+        encoding
+    end
+
+    private
+    def generate_short_url
+        self.short_url = self.random_code
+    end
+    
 end
--- practice-for-ch-sql-url-shortener-long-practice-main/URLShortener/db/migrate/20230303010543_create_shortened_urls.rb
+++ practice-for-ch-sql-url-shortener-long-practice-main/URLShortener/db/migrate/20230303010543_create_shortened_urls.rb
@@ -1,8 +1,12 @@
 class CreateShortenedUrls < ActiveRecord::Migration[7.0]
   def change
     create_table :shortened_urls do |t|
+      t.string :long_url, null: false
+      t.string :short_url
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
+    add_index :shortened_urls, :short_url, unique: true
   end
 end
